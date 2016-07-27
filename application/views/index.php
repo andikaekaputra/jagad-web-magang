d58a7f6803f6c3d0da9914b7bfa42312fbc26bb0
andikaekaputra <andiekap.95@gmail.com>
2016-07-28 00:33:20 +0700

    web profil
    commit to master

---
+++ application/views/index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Web profile</title>
+    <!--REQUIRED STYLE SHEETS-->
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="<?php echo base_url('assets/css/bootstrap.css');?>" rel="stylesheet" />
+    <!-- FONTAWESOME STYLE CSS -->
+    <link href="<?php echo base_url('assets/css/font-awesome.min.css');?>" rel="stylesheet" />
+    <!-- VEGAS STYLE CSS -->
+    <link href="<?php echo base_url('assets/scripts/vegas/jquery.vegas.min.css');?>" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="<?php echo base_url('assets/css/style.css" rel="stylesheet');?>" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css' />
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="htttp://www.binarytheme.com">DIKA </a>
+            </div>
+            <!-- Collect the nav links for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#home">HOME</a>
+                    </li>
+                    <li><a href="#about">PORTOFOLIO</a>
+                    </li>
+
+                    <li><a href="#price-sec">EXPERIENCE</a>
+                    </li>
+
+                    <li><a href="#contact-sec">CONTACT</a>
+                    </li>
+                    <li><a href="#"><strong>e-mail:</strong>  andiekap.95@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!--End Navigation -->
+
+
+    <!--Header section  -->
+    <div class="container" id="home">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1 class="head-main">ANDIKA EKA PUTRA</h1>
+                <h2 class="head-sub-main">Hello world i'm an Indonesian </h2>
+                <a class="btn btn-danger btn-lg " href="#about">Read More </a>
+                <div class="head-last"></div>
+
+            </div>
+
+
+        </div>
+    </div>
+    <!--End Header section  -->
+
+
+    <!--words Section-->
+    <section class="for-full-back color-white" id="about">
+        <div class="container">
+
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>PORTOFOLIO</h1>
+                    <h4>
+                        <strong>My Name is Andika Eka Putra, 
+                        I was born on 12 January 1995, I'm the first child of two siblings. Now i'm studying in Brawijaya University Malang as student of computer science.
+                        </strong>
+                    </h4>
+                </div>
+
+            </div>
+             <div class="row text-center">
+                <h2> Interested : <h2/>  
+
+            </div>
+            
+            <div class="row text-center space-pad">
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+                        <i class="fa fa-camera fa-4x main-color-red"></i>
+
+
+                        <h3>Photography </h3>
+                        <p>
+                            can operating a camera well, but not profesional photographer, just for fun.
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+                        <i class="fa fa-pencil fa-4x main-color-red"></i>
+
+
+                        <h3>Writing</h3>
+                        <p>
+                            Sometime writing a poem, an article or an opinion for jurnalistic product 
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+                        <i class="fa fa-desktop fa-4x main-color-red"></i>
+
+
+                        <h3>Design </h3>
+                        <p>
+                            usually design a layout from jurnalistic product or editing video.
+                        </p>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 ">
+
+                    <div class="about-div">
+
+                        <i class="fa fa-code fa-4x main-color-red"></i>
+
+
+                        <h3>Programming </h3>
+                        <p>
+                             i am Interested on IT world and some programing language
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+           
+        </div>
+
+
+    </section>
+
+    <section class="for-full-back " id="Parallax-one">
+        <div class="container">
+
+            <div class="row text-left for-parallax-one">
+                <div class="col-md-6 ">
+                    <h3>SKILLS</h3>
+
+                    <h4 align="left">
+                        <strong style="font-size:14px;padding-bottom:0px;">
+                            CODE
+                        </strong>
+                        <h3 align="left">HTML, PHP, CSS</h3>
+                     <h4/>   
+                        
+                        <h4 align="left">
+                        <strong style="font-size:14px;padding-bottom:0px;">
+                            TOOLS
+                        </strong>
+                        <h3 align="left">AI, PS, COREL, Sublime Text</h3>
+                        <h4/>   
+                </div>
+                <div class="col-md-6 ">
+                        <h3 align="right">EDUCATION</h3>
+                            <h4 align="right">
+                                <strong style="font-size:14px;padding-bottom:0px;">
+                                2001 - 2007
+                                 </strong>
+                        <h3 align="right">Sidokerto 1 Elementary School</h3>
+
+                        <h4 align="right">
+                                <strong style="font-size:14px;padding-bottom:0px;">
+                                2007 - 2010
+                                 </strong>
+                        <h3 align="right">Buduran 1 Junior High School</h3>
+                        <h4/>
+
+
+                        <h4 align="right">
+                                <strong style="font-size:14px;padding-bottom:0px;">
+                                 2010 - 2013
+                                 </strong>
+                        <h3 align="right">Antartika Senior High School</h3>
+                        <h4/>
+
+                        <h4 align="right">
+                                <strong style="font-size:14px;padding-bottom:0px;">
+                                 2013 - 2017
+                                 </strong>
+                        <h3 align="right">Brawijaya University</h3>
+                        <h4/>
+
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <!--./parallax one-->
+    <!--End words Section-->
+
+
+
+
+    <!-- EXPERIENCE -->
+
+    <section class="for-full-back color-white " id="price-sec">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1 align="left">EXPERIENCE</h1>
+                    <h4 align="left">
+                       <h3 align="left">Student Organization in Antartika Senior High school</h3>
+                        <h3 align="left">Jurnalistic Comunity in Antartika Senior High school</h3>
+                        <h3 align="left">Basketball Team in Antartika Senior High school</h3>
+                        <h3 align="left">DISPLAY Press Organization in Brawijaya University<h3>
+                     <h4/>   
+                </div>
+
+            
+    </section>
+
+    <section class="for-full-back for-parallax-two" id="Parallax-two">
+        <div class="container">
+
+            <div class="row text-right">
+                <div class="col-md-8 col-md-offset-2 ">
+                    <h1>DIKTUM</h1>
+                    <h4>
+                        <strong>apa yang bisa dibanggakan dari seorang kaum intelektual, jika bukan pemikiran dan pergerakannya. terlepas substansinya apa ! (Dika)
+                        </strong>
+                    </h4>
+                </div>
+
+            </div>
+
+
+        </div>
+    </section>
+    <!--./parallax two-->
+    <!--End Pricing Section -->
+
+    <!-- Contact Section -->
+    <section class="for-full-back color-white " id="contact-sec">
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-md-5 contact-cls">
+                    <h3>CONTACT ME </h3>
+                    <div>
+                        <span><i class="fa fa-home"></i>&nbsp; Address: MT.Haryono street Malang City</span>
+                        <br />
+                        <span><i class="fa fa-phone"></i>&nbsp;Phone: 088217058684</span>
+                        <br />
+                        <span><i class="fa fa-envelope-o"></i>&nbsp;E-Mail: andiekap.95@gmail.com</span>
+                        <br />
+                    </div>
+                    <br />
+                    <div id="social-icon">
+                        <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-linkedin fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+                    </div>
+                </div>
+               
+    </section>
+
+    <!--End Contact Section -->
+    <!--footer Section -->
+    <div class="for-full-back " id="footer">
+        2016 | All Right Reserved
+         
+    </div>
+    <!--End footer Section -->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="<?php echo base_url('assets/plugins/jquery-1.10.2.js');?>"></script>
+    <!-- BOOTSTRAP CORE SCRIPT-->
+    <script src="<?php echo base_url('assets/plugins/bootstrap.js');?>"></script>
+    <!-- VEGAS SLIDESHOW SCRIPTS -->
+    <script src="<?php echo base_url('assets/plugins/vegas/jquery.vegas.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/jquery.parallax-1.1.3.js');?>"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="<?php echo base_url('assets/js/custom.js');?>"></script>
+
+</body>
+</html>
